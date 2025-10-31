2ed865b54f5752f1cb714327dffdf318b7ae32c6
Veeresh Koliwad <157433566+veereshgit99@users.noreply.github.com>
2025-10-30 20:24:49 -0700

    Ui changes-1

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -29,23 +29,8 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
                 visible: { opacity: 1 },
             }}
             transition={{ delay: 0.1 }}
-            // @ts-ignore
-            onMouseMove={onMouseMove}
-            className="overflow-hidden relative duration-700 border rounded-xl hover:bg-background/80 group md:gap-8 hover:border-dark/50 border-dark"
+            className="overflow-hidden relative duration-300 border rounded-xl hover:border-secondary group md:gap-8 border-dark"
         >
-            <div className="pointer-events-none">
-                <div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
-                <motion.div
-                    // @ts-ignore
-                    className="absolute inset-0 z-10  bg-gradient-to-br opacity-100 via-blue-300/20  dark:via-zinc-100/10  transition duration-1000 group-hover:opacity-50"
-                    style={style}
-                />
-                <motion.div
-                    // @ts-ignore
-                    className="absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100"
-                    style={style}
-                />
-            </div>
             {children}
         </motion.div>
     );
